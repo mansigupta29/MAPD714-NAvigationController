992b322b80e8af636769290d5e19fc926405c181
Student <student@SETASs-Mac-mini.local>
2017-11-29 21:33:22 -0500

    modified font list  view controller

--- MPAD714-NavigationTableView/RootViewController.swift
+++ MPAD714-NavigationTableView/RootViewController.swift
@@ -66,4 +66,23 @@ class RootViewController: UITableViewController {
     }
     
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let indexPath =  tableView.indexPath(for: sender as! UITableViewCell)!
+        let listVC = segue.destination as! FontListViewController
+        
+        if indexPath.section == 0 {
+            let familyName = familyNames[indexPath.row]
+            listVC.fontNames = (UIFont.fontNames(forFamilyName: familyName) as [String]).sorted()
+            listVC.navigationItem.title =  familyName
+            listVC.showsFavourites = false
+        }else{
+        
+            listVC.fontNames = favouriteList.favourites
+            listVC.navigationItem.title = "Favourites"
+            listVC.showsFavourites = true
+        }
+    }
+    
+    
+    
 }
